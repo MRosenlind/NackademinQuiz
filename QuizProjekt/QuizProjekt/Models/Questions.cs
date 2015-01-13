0e82af05e4cd01246e159acbc160b43dbf82a544
plingplong <markus.rosenlind@gmail.com>
2015-01-13 13:49:33 +0100

    skapat classer

---
+++ QuizProjekt/QuizProjekt/Models/Questions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuizProjekt.Models
+{
+    public class Question
+    {
+        public string Text { get; set; }
+        public virtual ICollection<Alternative> Alternatives { get; set; }
+
+
+    }
+}
