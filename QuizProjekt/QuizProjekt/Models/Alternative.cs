14e190285b77799e1cac07c6473026f30b52b8fc
plingplong <markus.rosenlind@gmail.com>
2015-01-13 14:01:59 +0100

    go

---
+++ QuizProjekt/QuizProjekt/Models/Alternative.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuizProjekt.Models
+{
+    public class Alternative
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public bool Correct { get; set; }
+        public virtual Question Questions { get; set; }
+    }
+}
--- QuizProjekt/QuizProjekt/Models/Question.cs
+++ QuizProjekt/QuizProjekt/Models/Question.cs
@@ -7,8 +7,10 @@ namespace QuizProjekt.Models
 {
     public class Question
     {
+        public int Id { get; set; }
         public string Text { get; set; }
         public virtual ICollection<Alternative> Alternatives { get; set; }
+        public virtual Test Test { get; set; }
 
 
     }
