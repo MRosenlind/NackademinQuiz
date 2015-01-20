95330838236386cd2a7ae360863070e7582d6a54
plingplong <markus.rosenlind@gmail.com>
2015-01-20 11:18:03 +0100

    mm

--- QuizProjekt/QuizProjekt/Global.asax.cs
+++ QuizProjekt/QuizProjekt/Global.asax.cs
@@ -15,19 +15,19 @@ namespace QuizProjekt
     public class Global : System.Web.HttpApplication
     {
 
-        protected void Application_Start(object sender, EventArgs e)
-        {
-           //Database.SetInitializer(new MigrateDatabaseToLatestVersion<TestContext,QuizProjekt.Migrations.Configuration>());
-            Database.SetInitializer(new CreateDatabaseIfNotExists<TestContext>());
-           using (var ctx = new TestContext())
-           {
-               foreach (var test in ctx.Tests)
-               {
-                   Debug.Write(test.Name);
-               }
-           }
+        //protected void Application_Start(object sender, EventArgs e)
+        //{
+        //   //Database.SetInitializer(new MigrateDatabaseToLatestVersion<TestContext,QuizProjekt.Migrations.Configuration>());
+        //    Database.SetInitializer(new CreateDatabaseIfNotExists<TestContext>());
+        //   using (var ctx = new TestContext())
+        //   {
+        //       foreach (var test in ctx.Tests)
+        //       {
+        //           Debug.Write(test.Name);
+        //       }
+        //   }
 
-        }
+        //}
 
         protected void Session_Start(object sender, EventArgs e)
         {
