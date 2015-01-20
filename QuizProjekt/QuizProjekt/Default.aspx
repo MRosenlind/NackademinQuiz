459956c64547aad5c65496854096deafcfb4cfed
plingplong <markus.rosenlind@gmail.com>
2015-01-20 11:37:06 +0100

    nu kör vi

--- QuizProjekt/QuizProjekt/Default.aspx
+++ QuizProjekt/QuizProjekt/Default.aspx
@@ -1,44 +1,18 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="QuizProjekt.Default" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
-
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <div id="header">
-      <div id="h1">
-        MM Quizes
-      </div>  
-        
-    </div>
-    <div id="meny">
-        <asp:Button ID="btnLogin" runat="server"  OnClick="btnLogin_Click" Text="Logga In" />
-    </div>
-    
-    <div id="quiz-list">
-        <ul>
-            <li>En quiz <a href="#1">Starta</a></li>
-            <li>En annan <a href="#2">Starta</a></li>
-            <li>Fula frågor <a href="#3">Starta</a></li>
-        </ul>
-    </div>
-    
-    
-
-    
     <asp:Repeater ID="QuizList" runat="server">
         <ItemTemplate>
-            
-            <div id="QuizDiv">
+            <div>
                 <br />
-                <div id="ListQuiz"><a href="DoQuiz.aspx?id=<%#Eval("Id") %>"><%#Eval("Name") %> </a>
-                    <br />
-                    <%#Eval("Description") %>
-                    </div>
-                
+                <div><a href="DoQuiz.aspx?id=<%#Eval("Id") %>"><%#Eval("Name") %> 
+                     </a>
+                </div>
+                    <div><%#Eval("Description") %></div>
+                <br />
             </div>
         </ItemTemplate>
     </asp:Repeater>
-    <asp:SqlDataSource ID="SqlDataSource1" runat="server"></asp:SqlDataSource>
-    <br />
-    
        
 </asp:Content>
--- QuizProjekt/QuizProjekt/Repository/QuizRepository.cs
+++ QuizProjekt/QuizProjekt/Repository/QuizRepository.cs
@@ -7,80 +7,30 @@ using System.Web;
 
 namespace QuizProjekt.Repository
 {
-    public static class QuizRepository
+    public class QuizRepository
     {
-        public static List<Test> GetAllQuizes()
+        public List<Test> GetAllQuizes()
         {
-            List<Test> tests;
-            using (var ctx = new TestContext())
-            {
-                tests = ctx.Tests.ToList();
-            }
-            return tests;
+            var context = new TestContext();
+            return context.Tests.ToList();
+            
         }
-        public static Test AddQuiz(string name, string description, bool isPublic = true)
+        public void AddQuiz(Test quiz)
         {
-            var test = new Test
-            {
-                Name = name,
-                Description = description,
-                Public = isPublic,
-                Questions = new List<Question>()
-            };
-
-            using (var ctx = new TestContext())
-            {
-                ctx.Tests.Add(test);
-                ctx.SaveChanges();
-
-                return test;
-            }
+            var context = new TestContext();
+            context.Tests.Add(quiz);
+            context.SaveChanges();
         }
-        public static Question AddQuestion(string text, int testId)
+        public void AddQuestion(Question question, int testId)
         {
-            var question = new Question
+            var context = new TestContext();
+            var test = context.Tests.FirstOrDefault(x => x.Id == testId);
+            if (test!= null)
             {
-                Text = text,
-                Alternatives = new List<Alternative>()
-            };
-
-            using (var ctx = new TestContext())
-            {
-                var theTest = ctx.Tests.FirstOrDefault(t => t.Id == testId);
-                if (theTest == null) return null;
-
-                theTest.Questions.Add(question);
-                ctx.SaveChanges();
-
-                return question;
-
+                test.Questions.Add(question);
+                context.SaveChanges();
             }
-        }
-        public static Alternative AddAlternative(string text, int questionsId, bool correct = false)
-        {
-            // Skapa ett alternativ
-            var alternative = new Alternative
-            {
-                Text = text,
-                Correct = correct
-            };
 
-            // Ta fram fråga och lägg till alternativet i databasen.
-            using (var ctx = new TestContext())
-            {
-                var question = ctx.Questions.FirstOrDefault(x => x.Id == questionsId);
-                if (question != null)
-                {
-                    //alternative.Question = question;
-                    question.Alternatives.Add(alternative);
-
-                    // Genomför ändringarna i databasen
-                    ctx.SaveChanges();
-
-                    return alternative;
-                }
-                return null;
-            }
         }
 
     }
